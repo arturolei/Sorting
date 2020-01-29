d8b72cc189abceb2feb4d2c5b39c077edd6546d6
arturolei <artie.lei@gmail.com>
2020-01-29 00:14:18 -0500

    Changed comment wording; used alternate syntax for swapping variables to avoid using 'temp' variable

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -3,17 +3,15 @@ def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
-        smallest_index = cur_index
+        smallest_index = cur_index 
         # find next smallest element
         for j in range(cur_index, len(arr)):
             if arr[j] < arr[smallest_index]:
                 smallest_index = j
 
-        # swap
-        temp = arr[smallest_index]
-        arr[smallest_index] = arr[cur_index]
-        arr[cur_index] = temp
-
+        # swap them. Using different syntax to avoid having to use temp variables
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
+        
     return arr
 
 
