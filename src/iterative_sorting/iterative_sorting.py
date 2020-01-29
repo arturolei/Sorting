015625ffb1f999b24210c842a94a416ffbb3900c
arturolei <artie.lei@gmail.com>
2020-01-29 00:11:06 -0500

    Completed selection and bubble sort. Reviewing solution and adding notes.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,26 +1,36 @@
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
+    # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        curr_index = i
+        cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
+        # find next smallest element
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
-
-
-        # TO-DO: swap
-
-
-
+        # swap
+        temp = arr[smallest_index]
+        arr[smallest_index] = arr[cur_index]
+        arr[cur_index] = temp
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort( arr ):
-
+    swaps_happened = True #tests if we are swapping
+    while swaps_happened:
+        swaps_happened = False
+        # Loop through your array
+        for i in range(0, len(arr) - 1):
+            # Compare each element to its neighbor on the right
+            if arr[i] > arr[i+1]:
+                # If element on the right is smaller, swap them
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swaps_happened = True # Clearly a swap happened
+        # If swaps_happened is true, while loop condition holds
     return arr
 
 
