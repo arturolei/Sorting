b69dd876454c2c404b3617d2f2a2dabf52630c0b
arturolei <artie.lei@gmail.com>
2020-02-01 09:17:04 -0500

    Finishing up merge sorting

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,18 +1,47 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
+
+    elements = len( arrA ) + len( arrB ) #Get the total number of elements
     
+    merged_arr = [0] * elements # reate an empty list based on the size of elements
+    
+    #Initialize reference points to keep track of increments
+    a = 0
+    b = 0
+
+    # Go through the list and replace the 0's in merged_arr with the smallest element between arrA and arrB
+    for index in range(elements):
+        # Checks to see if the list is done sorting on side A and assigns the current index to the value of arrB[b]
+        if a >= len(arrA):
+            merged_arr[index] = arrB[b]
+            b += 1
+        # Checks to see if the list is done sorting on side B and assigns the current index to the value of arrA[a]
+        elif b >= len(arrB):
+            merged_arr[index] = arrA[a]
+            a += 1
+        # If the value of a is smaller, assign a to the merged_arr[index] and move on to its next reference point
+        elif arrA[a] < arrB[b]:
+            merged_arr[index] = arrA[a]
+            a += 1
+        # Otherwise, assign b to the merged_arr[index] and move on to its next reference point
+        else:
+            merged_arr[index] = arrB[b]
+            b += 1
+
+    # Returns the merged array
     return merged_arr
 
-
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
-
+def merge_sort( arr ):   
+    if len(arr) > 1:
+        # Get the middle (floor division)
+        middle = len(arr) // 2
+        # Get everything from the LHS and break it down until you get a list of length 1
+        left = merge_sort(arr[:middle])
+        # Get everything from the LHS and break it down until you get a list of length 1
+        right = merge_sort(arr[middle:])
+        # Re-assign arr
+        arr = merge(left, right)
     return arr
-
 
 # STRETCH: implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
